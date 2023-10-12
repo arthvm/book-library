2578f5420e8f42c61fcd1a4443685ac92dbd4605
Arthur Mariano <90999445+arthvm@users.noreply.github.com>
2023-10-11 21:26:06 -0300

    Add delete button in book card
    This commit adds a button in every book card that allows the user to
    delete it from the grid. It also removes it from the bookLibrary array.

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ formBtn.addEventListener("click", () => {
     bookRead.checked
   );
   addBookCard(bookLibrary[bookLibrary.length - 1]);
-  UpdateReadChecks();
+  UpdateListeners();
 });
 
 function ResetModal() {
@@ -36,15 +36,31 @@ function ResetModal() {
   bookRead.checked = false;
 }
 
-function UpdateReadChecks() {
-  readStatus = document.querySelectorAll(".read-status");
-  readStatus.forEach((element) =>
-    element.addEventListener("click", (e) => {
+function UpdateListeners() {
+  readStatusBtns = document.querySelectorAll(".read-status");
+
+  changeBtns = document.querySelectorAll(".change-btn");
+  deleteBtns = document.querySelectorAll(".delete-btn");
+
+  readStatusBtns.forEach((readStatusBtn) =>
+    readStatusBtn.addEventListener("click", (e) => {
       e.target.textContent =
         e.target.textContent == "Read" ? "To Read" : "Read";
       e.target.classList.toggle("read");
     })
   );
+
+  deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener("click", (e) => {
+      let parentEl = e.target.parentElement;
+      while (!parentEl.classList.contains("book-card")) {
+        parentEl = parentEl.parentElement;
+      }
+
+      let elementRemoved = bookLibrary.splice(parentEl.dataset.index);
+      parentEl.remove();
+    });
+  });
 }
 
 function Book(title, author, pages, read) {
@@ -60,7 +76,9 @@ function addBookToLibrary(bookTitle, bookAuthor, bookPages, wasRead) {
 }
 
 function addBookCard(book) {
-  cardsContainer.innerHTML += `<div class="book-card">
+  cardsContainer.innerHTML += `<div class="book-card" data-index=${
+    bookLibrary.length - 1
+  }>
     <h2 class="book-title">${book.title}</h2>
     <h3 class="book-author">by ${book.author}</h3>
     <p class="book-pages">${book.pages} pages</p>
@@ -68,6 +86,10 @@ function addBookCard(book) {
         <p class="read-status ${book.read == true ? "read" : ""}">${
     book.read == true ? "Read" : "To Read"
   }</p>
+    </div>
+    <div class="book-options">
+        <i class="fa-solid fa-gear fa-lg change-btn" style="color: #000000;"></i>
+        <i class="fa-solid fa-trash fa-lg delete-btn" style="color: #d8463b;"></i>
     </div>
   </div>`;
 }
